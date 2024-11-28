85169907e88d73e13701ee1f3c31ac539ab70a47
Omar Leal <Omar Leal>
2024-11-27 21:39:31 -0700

    first commit

---
+++ Programa mayor de edad.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int edad;
+    cout << "Ingrese un valor: ";
+    cin>>edad;
+    if(edad > 17)
+    {
+        cout<<"Es mayor de edad";
+    }else{
+        cout<<"Es menor de edad";
+    }
+
+
+    return 0;
+}
